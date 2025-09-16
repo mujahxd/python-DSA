b95c194b8e515ce6f481f7dccf55159feaf1b1aa
mujahxd <mujahid.170997@gmail.com>
2025-09-16 14:38:04 +0700

    tambah method pop pada double linked list

--- doubleLinkedList/main.py
+++ doubleLinkedList/main.py
@@ -30,8 +30,23 @@ class DoubleLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
 
 my_double_linked_list = DoubleLinkedList(7)
 my_double_linked_list.append(3)
-my_double_linked_list.append(2)
-my_double_linked_list.print_list()
+print(my_double_linked_list.pop())
+print(my_double_linked_list.pop())
+print(my_double_linked_list.pop())
