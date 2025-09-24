a4bc2c13cecf0b3951e5ba1a3808eaa5f93ab420
mujahxd <mujahid.170997@gmail.com>
2025-09-24 07:29:49 +0700

    tambah method pop pada stack

--- stack/main.py
+++ stack/main.py
@@ -28,8 +28,20 @@ class Stack:
             self.top = new_node
         self.height += 1
 
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
+
 
 my_stack = Stack(3)
 my_stack.push(2)
 my_stack.push(1)
+
+print(my_stack.pop(), "\n")
+
 my_stack.print_stack()
