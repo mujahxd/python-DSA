07eacb326ecab31db33f35baa810de0e77e19716
mujahxd <mujahid.170997@gmail.com>
2025-09-24 07:07:58 +0700

    tambah method push pada stack

--- stack/main.py
+++ stack/main.py
@@ -19,6 +19,17 @@ class Stack:
             print(temp)
             temp = temp.next
 
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
 
-my_stack = Stack(4)
+
+my_stack = Stack(3)
+my_stack.push(2)
+my_stack.push(1)
 my_stack.print_stack()
