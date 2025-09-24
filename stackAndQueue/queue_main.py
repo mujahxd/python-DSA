b82d1090cefdf1bc4980b12486d86eeb01b9cab6
mujahxd <mujahid.170997@gmail.com>
2025-09-24 07:56:23 +0700

    tambah method dequeue pada queue

--- stackAndQueue/queue_main.py
+++ stackAndQueue/queue_main.py
@@ -31,7 +31,23 @@ class Queue:
         self.length += 1
         return True
 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
 
 my_queue = Queue(1)
 my_queue.enqueue(2)
-my_queue.print_queue()
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+# my_queue.print_queue()
